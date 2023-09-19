5498dc8fa857db51967fb63b598c683d7f35a695
Juan Diego Rodriguez Steller <jdiego.rs@gmail.com>
2023-09-19 14:34:49 -0600

    Initial Commit

---
+++ Gymondo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gymondo
+//
+//  Created by Juan Diego Rodriguez Steller on 19/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
